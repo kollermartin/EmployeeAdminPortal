25c3a4f888c4f786a007dbdf08500bd20f446c9c
Martin Koller <kollermartin00@gmail.com>
2025-01-05 18:54:58 +0100

    feat: create dbContext, connection string

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
+{
+    public DbSet<Employee> Employees { get; set; }
+}
