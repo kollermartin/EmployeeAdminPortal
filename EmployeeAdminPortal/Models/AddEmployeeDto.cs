33d3ae8fa2ac42b250a9ea44119c6a877da8fc7c
Martin Koller <kollermartin00@gmail.com>
2025-01-06 19:50:00 +0100

    feat: Employee CRUD

---
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeAdminPortal.Models;
+
+public class AddEmployeeDto
+{
+    public required string Name { get; set; }
+    public required string Phone { get; set; }
+    public string? Email { get; set; }
+    public decimal Salary { get; set; }
+}
