1e71bd4a070ebb9e0b3ec9e8ec8ea33b0fa59ac5
Martin Koller <kollermartin00@gmail.com>
2025-01-06 19:08:51 +0100

    feat: gitignore + rady od špekouna

--- EmployeeAdminPortal/Program.cs
+++ EmployeeAdminPortal/Program.cs
@@ -3,12 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddEndpointsApiExplorer(); // Enables OpenAPI generation
-builder.Services.AddSwaggerGen(); // Adds Swagger generator
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
